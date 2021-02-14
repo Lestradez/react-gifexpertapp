db76200e7a7410de1f1a11b816d46ba96b009954
Lestradez <omarlestradez@gmail.com>
2021-02-14 13:02:47 -0300

    Testing GifGridItem

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-export const GifGridItem = ({id, title, url}) => {
-  console.log(id, title, url);
+
+export const GifGridItem = ({title, url}) => {
+  
   return (
     <div className = 'card animate__animated animate__fadeIn'>
     <img src = {url} alt = {title} />
@@ -9,3 +11,8 @@ export const GifGridItem = ({id, title, url}) => {
     </div>
   )
 }
+
+GifGridItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+  }
--- src/test/components/GifGridItem.test.js
+++ src/test/components/GifGridItem.test.js
@@ -4,9 +4,12 @@ import '@testing-library/jest-dom';
 import { GifGridItem } from "../../components/GifGridItem";
 
 describe ( 'Pruebas en <GifExpertApp />' , ()=>{
-  let wrapper = shallow( <GifGridItem/> );
+  const title = 'Título';
+  const url = 'http://localhost/unaurl.hml';
+
+  let wrapper = shallow( <GifGridItem title = {title} url = {url}/> );
     beforeEach(()=>{
-    wrapper = shallow( <GifGridItem/> );
+    wrapper = shallow( <GifGridItem title = {title} url = {url}/> );
   });
 
   test('Debe mostrar <GifGridItem /> correctamente', () => {
