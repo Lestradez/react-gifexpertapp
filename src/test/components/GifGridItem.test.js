21fe4efc10a4851c94384ba6658727ab99d9246d
Lestradez <omarlestradez@gmail.com>
2021-02-14 12:32:10 -0300

    Testing GifGridItem

--- src/test/components/GifGridItem.test.js
+++ src/test/components/GifGridItem.test.js
@@ -1,12 +1,12 @@
-import  GifExpertApp  from "../../GifExpertApp";
 import React from 'react';
 import { shallow } from "enzyme"; 
 import '@testing-library/jest-dom';
+import { GifGridItem } from "../../components/GifGridItem";
 
 describe ( 'Pruebas en <GifExpertApp />' , ()=>{
-  let wrapper = shallow( <GifExpertApp/> );
-   beforeEach(()=>{
-    wrapper = shallow( <GifExpertApp/> );
+  let wrapper = shallow( <GifGridItem/> );
+    beforeEach(()=>{
+    wrapper = shallow( <GifGridItem/> );
   });
 
   test('Debe mostrar <GifGridItem /> correctamente', () => {
